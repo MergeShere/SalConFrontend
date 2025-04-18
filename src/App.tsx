a6329819a8042dc4ce320cd2d82a6f6643e073f3
Evans Acheampong <evansachie01@gmail.com>
2025-04-18 19:14:47 +0000

    refactor: rename testimonial section

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-// src/App.tsx
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import Footer from './components/Footer'; // Import the Footer component
+import Footer from './components/Footer';
 import Featured from './components/home/Featured';
-import Testimonials from './components/home/testimonials';
+import Testimonials from './components/home/Testimonials';
 import HeroSection from './components/home/HeroSection';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/shared/ScrollToTop';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="flex flex-col min-h-screen">
-          <Navbar/>
-        {/* Your other app components would go here */}
-        <main className="flex-grow">
-          {/* Main content */}
-          <HeroSection/>
-          <Featured />
-          <Testimonials />
-        </main>
-        {/* Footer */}
-        <Footer />
-      </div>
+      <AnimatePresence>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          
+          <main className="flex-grow">
+            <HeroSection />
+            <Featured />
+            <Testimonials />
+          </main>
+
+          <Footer />
+
+          <ScrollToTop />
+        </div>
+      </AnimatePresence>
     </Provider>
   );
 }
