d8e9385275e526d0a3183f72c2cdae74eb0baec7
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-06-19 19:54:05 +0000

    Redux setup for Search in subsequent build

---
+++ src/redux/features/searchSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const initialState={};
+
+
+const searchSalonSlice = createSlice({
+    name:'search',
+    initialState,
+    reducers:{}
+});
+
+export const {} = searchSalonSlice.actions
+export default searchSalonSlice.reducer
