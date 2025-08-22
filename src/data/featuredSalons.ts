8d72c6150c4142b53e9844046751f10110aab601
Evans Acheampong <evansachie01@gmail.com>
2025-08-22 16:01:49 +0000

    feat: add data

---
+++ src/data/featuredSalons.ts
@@ -0,0 +1,56 @@
+export const featuredSalons = [
+  {
+    id: 'stone-ocean-salon',
+    name: 'Stone Ocean Salon',
+    rating: 4.8,
+    reviewCount: 245,
+    location: 'East Legon, Accra',
+    image: 'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  },
+  {
+    id: 'sailor-moon-salon',
+    name: 'Sailor Moon Hair Salon',
+    rating: 4.7,
+    reviewCount: 189,
+    location: 'Osu, Accra',
+    image: 'https://images.unsplash.com/photo-1522337360788-8b13dee7a37e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  },
+  {
+    id: 'the-world-hair-salon',
+    name: 'The World Hair Salon',
+    rating: 4.9,
+    reviewCount: 312,
+    location: 'Tema, Greater Accra',
+    image: 'https://images.unsplash.com/photo-1582095133179-bfd08e2fc6b3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  },
+  {
+    id: 'king-crimson-salon',
+    name: 'King Crimson Salon',
+    rating: 4.6,
+    reviewCount: 167,
+    location: 'Adabraka, Accra',
+    image: 'https://images.unsplash.com/photo-1633681926022-84c23e8cb2d6?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  },
+  {
+    id: 'golden-experience-salon',
+    name: 'Golden Experience Salon',
+    rating: 4.8,
+    reviewCount: 298,
+    location: 'Spintex, Accra',
+    image: 'https://images.unsplash.com/photo-1605497788044-5a32c7078486?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  },
+  {
+    id: 'star-platinum-salon',
+    name: 'Star Platinum Salon',
+    rating: 4.7,
+    reviewCount: 203,
+    location: 'Cantonments, Accra',
+    image: 'https://images.unsplash.com/photo-1620331311520-246422fd82f9?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit quis auctor odio arcu et dolor.'
+  }
+];
