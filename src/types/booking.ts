674dd49dc03d516c1e83326824920ceab0f5b49a
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-08-25 20:09:43 +0000

    Types for selection service

---
+++ src/types/booking.ts
@@ -0,0 +1,18 @@
+export interface HairServiceProps {
+  id: number;
+  name: string;
+  duration: string;
+  price: number;
+  category: string;
+}
+
+
+export interface ServiceSelectionState {
+  availableServices: HairServiceProps[];
+  selectedServices: HairServiceProps[];
+  subtotal: number;
+  tax: number;
+  total: number;
+  isModalOpen: boolean;
+  selectedServiceForModal: HairServiceProps | null;
+}
