fea7168612319f0e4e58176ede1c242a30b60f84
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-08-25 20:10:41 +0000

    Manage selection of service redux

---
+++ src/redux/features/serviceSelectionSlice.ts
@@ -0,0 +1,83 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ServiceSelectionState } from "../../types/booking";
+import { hairPriceServices } from "../../data/hairPriceData";
+
+
+const initialState:ServiceSelectionState= {
+    availableServices: hairPriceServices,
+    selectedServices: [],
+    subtotal: 0,
+    tax: 0,
+    total: 0,
+    isModalOpen: false,
+    selectedServiceForModal: null
+}
+
+
+const serviceSelectionSlice = createSlice({
+    name: 'selection',
+    initialState,
+    reducers: {
+        openModal: (state, action: PayloadAction<number>) => {
+            const serviceId = action.payload;
+            const service = state.availableServices.find(s => s.id === serviceId);
+
+            if (service) {
+                state.isModalOpen = true;
+                state.selectedServiceForModal = service;
+            }
+        },
+
+        closeModal: (state) => {
+            state.isModalOpen = false;
+            state.selectedServiceForModal = null;
+        },
+
+        addServiceToCart: (state) => {
+            if (state.selectedServiceForModal) {
+                const isAlreadySelected = state.selectedServices.find(
+                    s => s.id === state.selectedServiceForModal!.id
+                );
+
+                if (!isAlreadySelected) {
+                    state.selectedServices.push(state.selectedServiceForModal);
+                    serviceSelectionSlice.caseReducers.calculateTotals(state);
+                }
+
+                state.isModalOpen = false;
+                state.selectedServiceForModal = null;
+            }
+        },
+
+        removeService: (state, action: PayloadAction<number>) => {
+            const serviceId = action.payload;
+            state.selectedServices = state.selectedServices.filter(s => s.id !== serviceId);
+            serviceSelectionSlice.caseReducers.calculateTotals(state);
+        },
+
+        clearServices: (state) => {
+            state.selectedServices = [];
+            state.subtotal = 0;
+            state.tax = 0;
+            state.total = 0;
+        },
+
+        calculateTotals: (state) => {
+            // Fixed: service.price instead of serviceSelectionSlice.price
+            state.subtotal = state.selectedServices.reduce((total, service) => total + service.price, 0);
+            state.tax = 0;
+            state.total = state.subtotal + state.tax;
+        }
+    }
+});
+
+export const {
+    openModal,
+    closeModal,
+    addServiceToCart,
+    removeService,
+    clearServices,
+    calculateTotals
+} = serviceSelectionSlice.actions;
+
+export default serviceSelectionSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import searchReducer from './features/searchSlice';
+import serviceSelectionReducer from './features/serviceSelectionSlice'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    search: searchReducer
+    search: searchReducer,
+    selection: serviceSelectionReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch =()=>useDispatch<AppDispatch>();
+export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector;
